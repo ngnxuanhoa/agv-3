ee91eb8ee9647d07c5f9ae7f63ea5234363f467a
ngnxuanhoa <xuan.n.hoa@gmail.com>
2025-03-17 11:36:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ pipeline_str = (
     "image/jpeg,width=640,height=480,framerate=30/1 ! "
     "jpegenc ! "
     "rtpjpegpay ! "
-    "udpsink host=127.0.0.1 port=5000"  # Replace with the IP of your computer to test locally first
+    "udpsink host=192.168.1.60 port=5000"  # Replace with the IP of your computer to test locally first
 )
 # Initialize GStreamer
 Gst.init(None)
